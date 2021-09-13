388ed702ac8f52e9fb59d3fa35309cf93ba4583d
FahadCowboy <olivierafahad@gmail.com>
2021-09-13 17:39:50 +0600

    product layout modified

--- js/app.js
+++ js/app.js
@@ -2,26 +2,37 @@ const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => showProducts(data));
+    .then(data => showProducts(data));
 };
 loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+  console.log(products)
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.images;
     const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
+    div.classList = `product card-group`;
+    div.innerHTML = `
+
+
+
+
+      <div class="card py-4">
+        <img src="${product.image}" class="card-img-top w-50 mx-auto h-48" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-2xl">${product.title}</h5>
+          <p class="card-text">Category: ${product.category}</p>
+          <p class="card-text">Price: $ ${product.price}</p>
+        </div>
+        <div class="card-footer card-footer-modify">
+          <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <button type="button" onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="btn bg-purple-500 hover:bg-purple-600 text-white">Add to cart</button>
+            <button type="button" id="details-btn" class="btn bg-green-500 hover:bg-green-600 text-white">Details</button>
+            </div>
+        </div>
       </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -29,15 +40,21 @@ const showProducts = (products) => {
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
+
+  //only price amount will be updated by clicking 'add to cart' button.
   updatePrice("price", price);
 
   updateTaxAndCharge();
+
+  //Total prodact will be updated by addin 1 after each click on 'add to cart' button.
   document.getElementById("total-Products").innerText = count;
+
+  updateTotal()
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,7 +63,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -76,5 +93,6 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
